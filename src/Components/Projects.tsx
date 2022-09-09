2e7e6592f4e05ca319753ea6265ba9a5c34e140f
Jodi Chui <jodychui7@gmail.com>
2022-09-08 23:01:56 -0700

    Modal Works & is Reusable

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ProjectPineapple from "../Images/SC Project Pineapple.png"
 import ProjectAboutMe from "../Images/SC About Me Website 2.png"
 import ProjectFinLit from "../Images/SC FinLit Game.png"
 import ProjectSmartWalk from "../Images/SC SmartWalk.png"
+import Modal from './Modal'
 
 
 
 const Projects = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div id='Projects' className='w-full md:h-screen text-blue-700'>
         <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
@@ -15,6 +18,15 @@ const Projects = () => {
                 <h1 className='text-5xl text-center font-bold inline border-b-4 border-pink-600 2xl:text-7xl'>Projects</h1>
             </div>
             <h2 className='text-2xl mt-8 ml-4 text-orange-600 font-semibold md:my-8 2xl:text-3xl'>Check out some of my recent work</h2>
+
+
+            {/* testing modal */}
+            <button className="border-black border" onClick={() => setIsOpen(true)}>Open Modal</button>
+            {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}/>)}
+
+
+
+
             {/* end of title & used back quotes below (template literal tho)*/}
             <div style={{backgroundImage: `url(${ProjectPineapple})`}}
             className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
