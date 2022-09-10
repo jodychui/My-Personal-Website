70d271fa6af15c8a94e2e79489f8ccb3dd6f8664
Jodi Chui <jodychui7@gmail.com>
2022-09-09 19:15:54 -0700

    New Template Layout for Projects

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -4,13 +4,10 @@ import ProjectPineappleLogo from "../Images/ProjectPineappleLogo.jpeg"
 import ProjectAboutMe from "../Images/SC About Me Website 2.png"
 import ProjectFinLit from "../Images/SC FinLit Game.png"
 import ProjectSmartWalk from "../Images/SC SmartWalk.png"
-import Modal from './Modal'
 
 
 
 const Projects = () => {
-    const [isOpen, setIsOpen] = useState(false);
-
   return (
     <div id='Projects' className='bg-gray-50 w-full h-screen md:pb-16 text-blue-700'>
         <div className='max-w-[1000px] mx-auto flex-col w-full h-full'>
@@ -24,81 +21,39 @@ const Projects = () => {
 
 
             {/* TESTING project button & modal */}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
+            <div className='grid sm:grid-cols-2 gap-4'>
                 {/* project 1 */}
-                {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
+                <button className='border-4 border-[#e1bea2] rounded-lg'>
+                    {/* outer title */}
                     <h2 className='bg-blue-200 text-2xl text-center'>Project Pineapple</h2>
-                    {/* button container w/ hover effects*/}
-                    <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
-                    bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
-                        style={{backgroundImage: `url(${ProjectPineappleLogo})`}}>
-                        Click for more info
-                    </button>
-                    
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
-                    <div className='text-left mx-8 md:mx-10'>
+                    <img src={ProjectPineappleLogo} alt='Project Pineapple Logo'></img>
+                    <p className='text-black mt-1 text-lg ml-3 text-start'>Developed an IOS app that gives individuals the option of added security when going out 
+                            for the night by scheduling check-ins at certain times to survey the wellbeing of the user. If the 
+                            password is not submitted by the scheduled time, an alert will be sent to those that the user labeled 'contacts.'
+                        </p>
+                        <p className='ml-3 text-red-500 text-lg'>Won second place with this project at H4H 2022.</p>
+                        <img src={ProjectPineapple} alt='Project Pineapple Img' 
+                            className=' border-4 border-teal-600 rounded-lg mx-auto -mt-16 scale-50'>
+                        </img>
+
+                    {/* <button className="col-span-1 min-h-[500px] content-div shadow-lg shadow-[#7493b8] group container 
+                            rounded-md flex border justify-center bg-contain bg-no-repeat object-contain tracking-wider ">
+                        
+                        
                         <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Project Pineapple</h2>
                         <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Developed an IOS app that gives individuals the option of added security when going out 
                             for the night by scheduling check-ins at certain times to survey the wellbeing of the user. If the 
                             password is not submitted by the scheduled time, an alert will be sent to those that the user labeled 'contacts.'
                         </p>
                         <p className='text-red-500 text-base md:text-lg 2xl:text-xl'>Won second place with this project at H4H 2022.</p>
-                    </div>
                     <img src={ProjectPineapple} alt='Project Pineapple Img' 
-                        className=' border-4 border-teal-600 rounded-lg mx-auto mt-4 sm:mt-3 md:-mt-4 md:scale-[80%] lg:scale-[65%] lg:-mt-14 xl:-mt-18 xl:scale-[75%] 2xl:-mt-14 2xl:scale-[70%]'>
+                        className=' border-4 border-teal-600 rounded-lg mx-auto mt-4'>
                     </img>
-                    </Modal>)}
-                </div>
-                {/* project 2 */}
-                {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
-                    <h2 className='bg-orange-200 text-2xl text-center'>Personal Website</h2>
-                    {/* button container w/ hover effects*/}
-                    <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
-                    bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
-                        style={{backgroundImage: `url(${ProjectAboutMe})`}}>
-                        Click for more info
-                    </button>
-                    
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
-                    <div className='text-left mx-8 md:mx-10'>
-                        <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Personal Website</h2>
-                        <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Created own personal website to display 
-                        personal information, resume, programming projects, and contact information.
-                        </p>
-                    </div>
-                    <img src={ProjectPineapple} alt='Project Pineapple Img' 
-                        className=' border-4 border-teal-600 rounded-lg mx-auto mt-4 sm:mt-3 md:-mt-4 md:scale-[80%] lg:scale-[65%] lg:-mt-14 xl:-mt-18 xl:scale-[75%] 2xl:-mt-14 2xl:scale-[70%]'>
-                    </img>
-                    </Modal>)}
-                </div>
-                {/* project 3 */}
-                {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
-                    <h2 className='bg-blue-200 text-2xl text-center'>Financial Literacy Game</h2>
-                    {/* button container w/ hover effects*/}
-                    <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
-                    bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
-                        style={{backgroundImage: `url(${ProjectFinLit})`}}>
-                        Click for more info
-                    </button>
-                    
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
-                    <div className='text-left mx-8 md:mx-10'>
-                        <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Financial Literacy Game</h2>
-                        <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Constructed an educational game aimed to teach 
-                        kids ages 6-14 the importance of saving and investing money to solve the financial literacy crisis in the U.S. 
-                        </p>
-                    </div>
-                    <img src={ProjectPineapple} alt='Project Pineapple Img' 
-                        className=' border-4 border-teal-600 rounded-lg mx-auto mt-4 sm:mt-3 md:-mt-4 md:scale-[80%] lg:scale-[65%] lg:-mt-14 xl:-mt-18 xl:scale-[75%] 2xl:-mt-14 2xl:scale-[70%]'>
-                    </img>
-                    </Modal>)}
-                </div>
+                    </button> */}
+    
+                   
+                </button>
+                
                                 
             </div>
 
@@ -108,63 +63,6 @@ const Projects = () => {
 
 
 
-
-            {/* end of title & used back quotes below (template literal tho)*/}
-            <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                {/* project 1 */}
-                {/* card container (content-div is custom)*/}
-                <div style={{backgroundImage: `url(${ProjectPineapple})`}} className='shadow-lg shadow-[#7493b8] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-black tracking-wider'>
-                            My Project Name
-                        </span>
-                        <div className='pt-8'>
-                            {/* project will open in modal: */}
-                            <button className="border-black border" onClick={() => setIsOpen(true)}>Show me more!</button>
-                            {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
-                                <div className='text-left mx-8 md:mx-10'>
-                                    <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Project Pineapple</h2>
-                                    <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Developed an IOS app that gives individuals the option of added security when going out 
-                                    for the night by scheduling check-ins at certain times to survey the wellbeing of the user. If the 
-                                    password is not submitted by the scheduled time, an alert will be sent to those that the user labeled 
-                                    'contacts.'</p>
-                                    <p className='text-red-500 text-base md:text-lg 2xl:text-xl'>Won second place with this project at H4H 2022.</p>
-                                </div>
-                                <img src={ProjectPineapple} alt='Project Pineapple Img' 
-                                    className=' border-4 border-teal-600 rounded-lg mx-auto mt-4 sm:mt-3 md:-mt-4 md:scale-[80%] lg:scale-[65%] lg:-mt-14 xl:-mt-18 xl:scale-[75%] 2xl:-mt-14 2xl:scale-[70%]'>
-                                </img>
-                            </Modal>)}
-                        </div>
-                    </div>
-                </div>
-
-                {/* project 2 */}
-                {/* card container (content-div is custom)*/}
-                <div style={{backgroundImage: `url(${ProjectPineapple})`}} className='shadow-lg shadow-[#7493b8] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects */}
-                    <div className='opacity-0 group-hover:opacity-100'>
-                        <span className='text-2xl font-bold text-black tracking-wider'>
-                            My Project Name
-                        </span>
-                        <div className='pt-8'>
-                            {/* project will open in modal: */}
-                            <button className="border-black border" onClick={() => setIsOpen(true)}>Show me more!</button>
-                            {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
-                                <div className='text-left mx-8 md:mx-10'>
-                                    <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Personal Website</h2>
-                                    <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Created own personal website to display 
-                                        personal information, resume, programming projects, and contact information.
-                                    </p>
-                                </div>
-                                <img src={ProjectPineapple} alt='Project Pineapple Img' 
-                                    className=' border-4 border-teal-600 rounded-lg mx-auto mt-4 sm:mt-3 md:-mt-4 md:scale-[80%] lg:scale-[65%] lg:-mt-14 xl:-mt-18 xl:scale-[75%] 2xl:-mt-14 2xl:scale-[70%]'>
-                                </img>
-                            </Modal>)}
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
         
     </div>
