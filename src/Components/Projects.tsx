f9152c7c2775b44fadc9cbe639dac352392f7a1f
Jodi Chui <jodychui7@gmail.com>
2022-09-13 14:38:21 -0700

    Fixed Modal
    Adding different children to the modal will now display correctly (not just one specific child)

--- src/Components/Projects.tsx
+++ src/Components/Projects.tsx
@@ -9,7 +9,7 @@ import Modal from './Modal'
 
 
 const Projects = () => {
-    const [isOpen, setIsOpen] = useState(false);
+    const [isOpen, setIsOpen] = useState(0);
 
   return (
     <div id='Projects' className='bg-gray-50 w-full h-screen md:pb-16 text-blue-700'>
@@ -27,17 +27,17 @@ const Projects = () => {
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
                 {/* project 1 */}
                 {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
+                <div className=''>
                     <h2 className='bg-blue-200 text-2xl text-center'>Project Pineapple</h2>
                     {/* button container w/ hover effects*/}
                     <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
                     bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
+                        onClick={() => setIsOpen(1)} 
                         style={{backgroundImage: `url(${ProjectPineappleLogo})`}}>
                         Click for more info
                     </button>
                     
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
+                    {isOpen == 1 && (<Modal setIsOpen={() => setIsOpen(0)}>
                     <div className='text-left mx-8 md:mx-10'>
                         <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Project Pineapple</h2>
                         <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Developed an IOS app that gives individuals the option of added security when going out 
@@ -53,17 +53,17 @@ const Projects = () => {
                 </div>
                 {/* project 2 */}
                 {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
+                <div className=''>
                     <h2 className='bg-orange-200 text-2xl text-center'>Personal Website</h2>
                     {/* button container w/ hover effects*/}
                     <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
                     bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
+                        onClick={() => setIsOpen(2)} 
                         style={{backgroundImage: `url(${ProjectAboutMe})`}}>
                         Click for more info
                     </button>
                     
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
+                    {isOpen == 2 && (<Modal setIsOpen={() => setIsOpen(0)}>
                     <div className='text-left mx-8 md:mx-10'>
                         <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Personal Website</h2>
                         <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Created own personal website to display 
@@ -77,17 +77,17 @@ const Projects = () => {
                 </div>
                 {/* project 3 */}
                 {/* title and button that shows modal */}
-                <div className='hover:opacity-70'>
+                <div className=''>
                     <h2 className='bg-blue-200 text-2xl text-center'>Financial Literacy Game</h2>
                     {/* button container w/ hover effects*/}
                     <button className="content-div shadow-lg shadow-[#7493b8] group container rounded-md flex border justify-center 
                     bg-contain bg-no-repeat object-contain tracking-wider" 
-                        onClick={() => setIsOpen(true)} 
+                        onClick={() => setIsOpen(3)} 
                         style={{backgroundImage: `url(${ProjectFinLit})`}}>
                         Click for more info
                     </button>
                     
-                    {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
+                    {isOpen == 3 && (<Modal setIsOpen={() => setIsOpen(0)}>
                     <div className='text-left mx-8 md:mx-10'>
                         <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Financial Literacy Game</h2>
                         <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Constructed an educational game aimed to teach 
@@ -109,20 +109,20 @@ const Projects = () => {
 
 
 
-            {/* end of title & used back quotes below (template literal tho)*/}
+            {/* end of title & used back quotes below (template literal tho) */}
             <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-                {/* project 1 */}
-                {/* card container (content-div is custom)*/}
+                project 1
+                {/* card container (content-div is custom) */}
                 <div style={{backgroundImage: `url(${ProjectPineapple})`}} className='shadow-lg shadow-[#7493b8] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects */}
+                    hover effects
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-black tracking-wider'>
                             My Project Name
                         </span>
                         <div className='pt-8'>
-                            {/* project will open in modal: */}
-                            <button className="border-black border" onClick={() => setIsOpen(true)}>Show me more!</button>
-                            {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
+                            project will open in modal:
+                            <button className="border-black border" onClick={() => setIsOpen(4)}>Show me more!</button>
+                            {isOpen == 4 && (<Modal setIsOpen={() => setIsOpen(0)}>
                                 <div className='text-left mx-8 md:mx-10'>
                                     <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Project Pineapple</h2>
                                     <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Developed an IOS app that gives individuals the option of added security when going out 
@@ -142,15 +142,15 @@ const Projects = () => {
                 {/* project 2 */}
                 {/* card container (content-div is custom)*/}
                 <div style={{backgroundImage: `url(${ProjectPineapple})`}} className='shadow-lg shadow-[#7493b8] group container rounded-md flex justify-center items-center mx-auto content-div'>
-                    {/* hover effects */}
+                    hover effects
                     <div className='opacity-0 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-black tracking-wider'>
                             My Project Name
                         </span>
                         <div className='pt-8'>
                             {/* project will open in modal: */}
-                            <button className="border-black border" onClick={() => setIsOpen(true)}>Show me more!</button>
-                            {isOpen && (<Modal setIsOpen={() => setIsOpen(false)}>
+                            <button className="border-black border" onClick={() => setIsOpen(5)}>Show me more!</button>
+                            {isOpen == 5 && (<Modal setIsOpen={() => setIsOpen(0)}>
                                 <div className='text-left mx-8 md:mx-10'>
                                     <h2 className='text-black text-center text-2xl sm:text-3xl md:text-4xl lg:text-5xl'>Personal Website</h2>
                                     <p className='text-black mt-3 text-lg md:text-xl 2xl:text-2xl'>Created own personal website to display 
