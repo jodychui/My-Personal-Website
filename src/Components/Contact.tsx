20286a7967df2a1dde719f25863ff482b82beb53
jodichui <jodychui7@gmail.com>
2022-12-27 19:28:28 -0800

    Added text box submission field to contact page
    used getform.io

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,7 +5,7 @@ import { AiOutlineLinkedin } from "react-icons/ai";
 
 const Contact = () => {
     return (
-        <div id='Contact' className='bg-[#D1CAC1] w-full h-screen md:pb-16 text-black-700'>
+        <div id='Contact' className='bg-[#D1CAC1] w-full pb-16 md:pb-16 text-black-700'>
             {/* title */}
             <div className='pt-8 md:pt-16 mx-auto flex-col text-center'>
                 <h1 className='text-5xl text-center font-bold inline border-b-4 border-pink-600 2xl:text-7xl'>Contact</h1>
@@ -34,6 +34,24 @@ const Contact = () => {
                     <h3 className='text-center text-2xl'>Lets Connect!</h3>
                 </div>
             </div>
+
+
+            {/* user fill in form */}
+            <div className='flex justify-center items-center p-4 mt-12'>
+                <form method="POST" action="https://getform.io/f/b1c8df9d-93f0-4bf3-9046-8afff62a6b11" className='max-w-[600px] w-full flex flex-col'>
+                    {/* submission box text */}
+                    <div className='text-xl text-amber-600 font-medium'>
+                        <p>Submit the form below or click the email icon above to shoot me an email</p>
+                    </div>
+                    <input className='p-2' type="text" placeholder='Name' name='name'/>
+                    <input className='my-4 p-2' type="email" placeholder='Email' name='email'/>
+                    <textarea className='p-2' placeholder='Message' name="message" rows={10}></textarea>
+
+                    <button className='text-black border-2 hover:bg-pink-500 hover:border-pink-500 px-4 py-3 my-8 mx-auto flex items-center'>Send message!</button>
+                </form>
+            </div>
+            
+
 
             {/* black bar on the bottom */}
            <div className='border-solid border-2 border-black bg-black text-right p-3 fixed inset-x-0 bottom-0'>
